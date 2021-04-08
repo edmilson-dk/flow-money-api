fea85de0d51989bd3f506a466a58e4b4d7e8a724
edmilson-dk <droidvidaboa@gmail.com>
2021-04-08 19:27:56 -0300

    feat: created conection with database

---
+++ src/drivers/db/knex/postgres/index.ts
@@ -0,0 +1,12 @@
+const environment: string = String(process.env.NODE_ENV) || 'development';
+
+import knexfile from '../../../../../knexfile';
+import knex from 'knex';
+
+const knexConfig = environment === 'development' 
+  ? knexfile.development 
+  : knexfile.production;
+
+const db = knex(knexConfig);
+
+export default db;
