c2b908f56255dc7a3a0580ab3bfed8879d35941a
edmilson-dk <droidvidaboa@gmail.com>
2021-04-08 19:17:09 -0300

    feat: created balance migration

---
+++ src/drivers/db/knex/postgres/migrations/20210408213925_balance.ts
@@ -0,0 +1,26 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  knex.schema.hasTable('balances').then(exists => {
+    if (!exists) {
+      return knex.schema.createTable('balances', table => {
+        table.decimal('joined').notNullable();
+        table.decimal('left').notNullable();
+        table.decimal('total').notNullable().unique();
+        table.string('password', 80).notNullable();
+
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+        table.string('user_id')
+          .references('users.id')
+          .notNullable()
+          .onDelete('CASCADE');
+      });
+    }
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+   knex.schema.dropTableIfExists('balances');
+}
