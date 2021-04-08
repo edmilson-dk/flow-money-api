6e2d9a9feadc68d591a8b3b7edf43499f035fdad
edmilson-dk <droidvidaboa@gmail.com>
2021-04-08 19:29:17 -0300

    feat: created balance migration

--- src/drivers/db/knex/postgres/migrations/20210408213925_balance.ts
+++ src/drivers/db/knex/postgres/migrations/20210408213925_balance.ts
@@ -22,5 +22,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-   knex.schema.dropTableIfExists('balances');
+  knex.schema.dropTableIfExists('balances');
 }
