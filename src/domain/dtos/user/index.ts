a52dc09520c851955790569ea02938fd3cad487b
edmilson-dk <droidvidaboa@gmail.com>
2021-04-10 16:56:52 -0300

    feat: created user dtos

---
+++ src/domain/dtos/user/index.ts
@@ -0,0 +1,20 @@
+export type UserDomainDTO = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export type UserPersistDTO = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export type UserDTO = {
+  id: string;
+  name: string;
+  email: string;
+  token: string;
+}
