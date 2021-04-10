37e5077588b2920635ca5156bfd0f96a1dd7707b
edmilson-dk <droidvidaboa@gmail.com>
2021-04-10 16:56:23 -0300

    feat: created user dtos

---
+++ src/domain/dtos/balance/index.ts
@@ -0,0 +1,18 @@
+export type BalanceDomainDTO = {
+  joined: number;
+  left: number;
+  total: number;
+}
+
+export type BalancePersistDTO = {
+  joined: number;
+  left: number;
+  total: number;
+  userId: string;
+}
+
+export type BalanceDTO = {
+  joined: number;
+  left: number;
+  total: number;
+}
