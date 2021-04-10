c037f83b76b7d4713f7a259d345e82ba0c80fe78
edmilson-dk <droidvidaboa@gmail.com>
2021-04-10 17:01:10 -0300

    refactor: added user persist dto

--- src/domain/use-cases/user/index.ts
+++ src/domain/use-cases/user/index.ts
@@ -1,6 +1,6 @@
-import { UserDTO } from "../../entities/dtos/user";
+import { UserPersistDTO } from "../../dtos/user";
 
 export interface IUserUseCases {
-  add: ({ id, name, email, password }: UserDTO) => Promise<void>;
-  findUserByEmail: (email: string) => Promise<UserDTO | {}>;
+  add: ({ id, name, email, password }: UserPersistDTO) => Promise<void>;
+  findUserByEmail: (email: string) => Promise<UserPersistDTO | {}>;
 }
