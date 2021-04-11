932225348948e8f5e771e54c4719ca6de00eec15
edmilson-dk <droidvidaboa@gmail.com>
2021-04-11 11:10:33 -0300

    refactor: removed unused configs

--- src/drivers/db/knex/postgres/index.ts
+++ src/drivers/db/knex/postgres/index.ts
@@ -1,6 +1,6 @@
 const environment: string = String(process.env.NODE_ENV) || 'development';
 
-import knexfile from '../../../../../knexfile';
+import knexfile from "../../../../../knexfile";
 import knex from 'knex';
 
 const knexConfig = environment === 'development' 
