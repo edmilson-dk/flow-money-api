c8a2774d53ba1c9b23808c251810506ccf2f4cd0
edmilson-dk <droidvidaboa@gmail.com>
2021-04-13 09:41:01 -0300

    refactor: added user persist dto

--- src/application/use-cases/user/index.ts
+++ src/application/use-cases/user/index.ts
@@ -1,6 +1,11 @@
 import { UserPersistDTO } from "../../../domain/dtos/user";
 import User from "../../../domain/entities/User";
+import { InvalidEmailError } from "../../../domain/entities/User/errors/email-error";
+import { InvalidIdError } from "../../../domain/entities/User/errors/id-error";
+import { InvalidNameError } from "../../../domain/entities/User/errors/name-error";
+import { InvalidPasswordError } from "../../../domain/entities/User/errors/password-error";
 import { IUserUseCases } from "../../../domain/use-cases/user";
+import { Either, left } from "../../../shared/either";
 import { IUserRepository } from "../../repositories/user";
 
 class UserUseCases implements IUserUseCases {
@@ -10,9 +15,20 @@ class UserUseCases implements IUserUseCases {
     this.userRepository = userRepository;
   }
 
-  async add(data: UserPersistDTO): Promise<void> {
-    const user = new User({ ...data });
-    await this.userRepository.add(user.getValues());
+  async add(data: UserPersistDTO): Promise<any> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError | InvalidIdError | InvalidPasswordError, User>  = User.create({ ...data });
+    
+    if (userOrError.isLeft()) {
+      return left(userOrError.value);
+    }
+    const user: User = userOrError.value;
+
+    await this.userRepository.add({
+      id: user.id.value,
+      email: user.email.value,
+      name: user.name.value,
+      password: user.password.value,
+    });
 
     return;
   }
