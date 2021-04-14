f4e52ae596eb91c86f71ddac5ffcb8c3dbb93bb3
edmilson-dk <droidvidaboa@gmail.com>
2021-04-14 09:36:39 -0300

    feat: created user use cases add user method

--- src/application/use-cases/user/index.ts
+++ src/application/use-cases/user/index.ts
@@ -1,12 +1,16 @@
-import { UserPersistDTO } from "../../../domain/dtos/user";
+import { UserDataDTO, UserDomainDTO, UserPersistDTO } from "../../../domain/dtos/user";
 import User from "../../../domain/entities/User";
 import { InvalidEmailError } from "../../../domain/entities/User/errors/email-error";
 import { InvalidIdError } from "../../../domain/entities/User/errors/id-error";
 import { InvalidNameError } from "../../../domain/entities/User/errors/name-error";
 import { InvalidPasswordError } from "../../../domain/entities/User/errors/password-error";
+import { AddUserResponse } from "./responses/add-user-response";
 import { IUserUseCases } from "../../../domain/use-cases/user";
-import { Either, left } from "../../../shared/either";
+import { Either, left, right } from "../../../shared/either";
+import { generateId } from "../../../utils/generateId";
 import { IUserRepository } from "../../repositories/user";
+import { AlredyExistsUserError } from "./errors/exists-user-error";
+import { encryptData } from "../../../utils/hash-bcrypt";
 
 class UserUseCases implements IUserUseCases {
   private userRepository: IUserRepository;
@@ -15,27 +19,33 @@ class UserUseCases implements IUserUseCases {
     this.userRepository = userRepository;
   }
 
-  async add(data: UserPersistDTO): Promise<any> {
-    const userOrError: Either<InvalidNameError | InvalidEmailError | InvalidIdError | InvalidPasswordError, User>  = User.create({ ...data });
+  async add(data: UserDataDTO): Promise<AddUserResponse> {
+    const id = generateId();
+    const userOrError: Either<InvalidNameError | InvalidEmailError | InvalidIdError | InvalidPasswordError, User>  = User.create({ id, ...data });
     
     if (userOrError.isLeft()) {
       return left(userOrError.value);
     }
-    const user: User = userOrError.value;
 
-    await this.userRepository.add({
+    const user: User = userOrError.value;
+    const existUser = await this.userRepository.existUserByEmail(user.email.value);
+
+    if (existUser) {
+      return left(new AlredyExistsUserError(user.email.value));
+    }
+
+    const hashPassword = await encryptData(user.password.value, 10);
+
+    const userData = {
       id: user.id.value,
       email: user.email.value,
       name: user.name.value,
-      password: user.password.value,
-    });
+      password: hashPassword,
+    }
 
-    return;
-  }
+    await this.userRepository.add(userData);
 
-  async findUserByEmail(email: string): Promise<{} | UserPersistDTO> {
-    const user = await this.userRepository.findUserByEmail(email);
-    return user;
+    return right(userData);
   }
 }
 
