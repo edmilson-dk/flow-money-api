780a4c6207d6c4983b92290df52b36b1d783aea0
edmilson-dk <droidvidaboa@gmail.com>
2021-04-14 09:38:39 -0300

    feat: added app routes

--- src/main/app/index.ts
+++ src/main/app/index.ts
@@ -1,6 +1,7 @@
 import express, { urlencoded, json } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import routes from "../routes/express";
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(cors());
 app.use(helmet());
 app.use(urlencoded({ extended: true }));
 app.use(json());
+
+app.use("/api", routes);
 
 export default app;
