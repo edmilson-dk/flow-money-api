e5f39592df5745337ad0c22778cec4a946d65f79
edmilson-dk <droidvidaboa@gmail.com>
2021-04-14 09:41:25 -0300

    feat: created add user controller

---
+++ src/adapters/presentation/controllers/user/add-user-controller.ts
@@ -0,0 +1,41 @@
+import { IUserUseCases } from "../../../../domain/use-cases/user";
+import { badRequest, ok, serverError } from "../../http/http-response-type";
+import { HttpRequest, HttpResponse } from "../../http/ports/http";
+import { MissingParamError } from "../errors/missing-params-error";
+import { AddUserResponse } from "../../../../application/use-cases/user/responses/add-user-response";
+import { createJWT } from "../../../../infra/jwt-token";
+import UserMap from "../../../../domain/dtos/user/user-map";
+import { BaseController } from "../baseControler";
+
+class AddUserController implements BaseController {
+  private readonly userUseCases: IUserUseCases;
+
+  constructor(userUseCases: IUserUseCases) {
+    this.userUseCases  = userUseCases;
+  }
+
+  async execute(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { name, email, password } = httpRequest.body;
+
+      if (!name || !email || !password) {
+        return badRequest(new MissingParamError(), 401);
+      }
+      
+      const addUserResponse: AddUserResponse = await this.userUseCases.add({ name, email, password });
+
+      if (addUserResponse.isLeft()) {
+        return badRequest(addUserResponse.value, 401);
+      }
+
+      const token = createJWT(addUserResponse.value.email, addUserResponse.value.id, '3d');
+      const userDTO = UserMap.toDTO({ token, ...addUserResponse.value });
+
+      return ok(userDTO, 201);
+    } catch (err) {
+      return serverError(err.message);
+    }
+  }
+}
+
+export default AddUserController;
