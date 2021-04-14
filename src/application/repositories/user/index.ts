1253dfbf2774ec5bf7f42b342452a02ef4525ce0
edmilson-dk <droidvidaboa@gmail.com>
2021-04-14 09:36:18 -0300

    refactor: renamed user repository method

--- src/application/repositories/user/index.ts
+++ src/application/repositories/user/index.ts
@@ -1,6 +1,6 @@
 import { UserPersistDTO } from "../../../domain/dtos/user";
 
 export interface IUserRepository {
-  add: ({ id, name, email, password }: UserPersistDTO) => Promise<void>;
-  findUserByEmail: (email: string) => Promise<UserPersistDTO | {}>;
+  add: (data: UserPersistDTO) => Promise<void>;
+  existUserByEmail: (email: string) => Promise<boolean>;
 }
