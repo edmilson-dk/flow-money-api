a6d5672727b16a63a16ab3aca269e7c86e719d04
edmilson-dk <droidvidaboa@gmail.com>
2021-04-14 09:45:55 -0300

    feat: created add user controller factory

---
+++ src/main/factories/user/add-user-factory.ts
@@ -0,0 +1,12 @@
+import { BaseController } from "../../../adapters/presentation/controllers/baseControler";
+import AddUserController from "../../../adapters/presentation/controllers/user/add-user-controller";
+import UserUseCases from "../../../application/use-cases/user";
+import UserRepository from "../../../infra/repositories/postgres/knex/user";
+
+export function makeAddUserController(): BaseController {
+  const userRepository = new UserRepository();
+  const userUseCases = new UserUseCases(userRepository);
+  const addUserController = new AddUserController(userUseCases);
+
+  return addUserController;
+}
