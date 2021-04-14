e061050a79161cf5bfe9b0a3a8b44e117187edc5
edmilson-dk <droidvidaboa@gmail.com>
2021-04-14 09:45:21 -0300

    feat: created express adapter

---
+++ src/main/adapters/express-adapter.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { BaseController } from "../../adapters/presentation/controllers/baseControler";
+import { HttpRequest } from "../../adapters/presentation/http/ports/http";
+
+export const adaptRoute = (controller: BaseController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      params: req.params,
+      query: req.query,
+    };
+    
+    const httpResponse = await controller.execute(httpRequest);
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+  }
+}
