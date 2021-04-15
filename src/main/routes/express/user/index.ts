5ff7a7d7f991d45c986903f55059b8595e6a2883
edmilson-dk <droidvidaboa@gmail.com>
2021-04-15 18:48:04 -0300

    feat: created login user router

--- src/main/routes/express/user/index.ts
+++ src/main/routes/express/user/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { adaptRoute } from "../../../adapters/express-adapter";
-import { makeAddUserController } from "../../../factories/user";
+import { makeAddUserController, makeGetUserController } from "../../../factories/user";
 
 const userRoutes = Router();
 
 userRoutes.post("/register", adaptRoute(makeAddUserController()));
+userRoutes.post("/login", adaptRoute(makeGetUserController()));
 
 export default userRoutes;
