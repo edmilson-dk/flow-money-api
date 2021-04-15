6009f64e205418d2b5043dbdbb8a5a858057b7e4
edmilson-dk <droidvidaboa@gmail.com>
2021-04-15 18:44:47 -0300

    feat: create get user repository method signature

--- src/application/repositories/user/index.ts
+++ src/application/repositories/user/index.ts
@@ -2,5 +2,6 @@ import { UserPersistDTO } from "../../../domain/dtos/user";
 
 export interface IUserRepository {
   add: (data: UserPersistDTO) => Promise<void>;
+  getUser: (email: string) => Promise<UserPersistDTO>;
   existUserByEmail: (email: string) => Promise<boolean>;
 }
