869ccea81ba9b11c20e000780ddee45fa0cc9127
edmilson-dk <droidvidaboa@gmail.com>
2021-04-15 18:45:38 -0300

    feat: created get user use case signature

--- src/domain/use-cases/user/index.ts
+++ src/domain/use-cases/user/index.ts
@@ -1,6 +1,8 @@
 import { AddUserResponse } from "../../../application/use-cases/user/responses/add-user-response";
+import { GetUserResponse } from "../../../application/use-cases/user/responses/get-user-response";
 import { UserDataDTO } from "../../dtos/user";
 
 export interface IUserUseCases {
   add: (data: UserDataDTO) => Promise<AddUserResponse>;
+  getUser: (email: string, password: string) => Promise<GetUserResponse>;
 }
