211d13f96281346010dcd25dd6b6e3a1d3bc0a28
edmilson-dk <droidvidaboa@gmail.com>
2021-04-16 16:52:08 -0300

    test: created test "Should not create name with more than 255 characters"

--- src/domain/entities/User/values-object/name.spec.ts
+++ src/domain/entities/User/values-object/name.spec.ts
@@ -4,4 +4,8 @@ describe("Name validator", () => {
   test("Should create name with minimum 4 characters", () => {
     expect(Name.validate("ed")).toBe(false);
   });
+
+  test("Should not create name with more than 255 characters", () => {
+    expect(Name.validate(`e`.repeat(256))).toBe(false);
+  });
 });
