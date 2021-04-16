c28ada3e98ec17235b2c4b60beefc466784c80f5
edmilson-dk <droidvidaboa@gmail.com>
2021-04-16 16:58:09 -0300

    test: created test "Should not create password with empty values"

--- src/domain/entities/User/values-object/password.spec.ts
+++ src/domain/entities/User/values-object/password.spec.ts
@@ -4,4 +4,8 @@ describe("Password validator", () => {
   test("Should not create password with minimum 6 characters", () => {
     expect(Password.validate("12345")).toBe(false);
   });
+
+  test("Should not create password with empty values", () => {
+    expect(Password.validate("")).toBe(false);
+  });
 });
