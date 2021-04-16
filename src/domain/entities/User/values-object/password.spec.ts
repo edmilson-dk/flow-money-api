57bb7d01fdb3cf3f54570737c3c0583324bdaf82
edmilson-dk <droidvidaboa@gmail.com>
2021-04-16 17:00:11 -0300

    test: created test "Should not create password with more than 255 characters"

--- src/domain/entities/User/values-object/password.spec.ts
+++ src/domain/entities/User/values-object/password.spec.ts
@@ -8,4 +8,8 @@ describe("Password validator", () => {
   test("Should not create password with empty values", () => {
     expect(Password.validate("")).toBe(false);
   });
+
+  test("Should not create password with more than 255 characters", () => {
+    expect(Password.validate("1".repeat(256))).toBe(false);
+  });
 });
