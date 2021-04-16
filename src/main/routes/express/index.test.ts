2e70df8d11b5606c65ee9e0635c9dcdd016db30f
edmilson-dk <droidvidaboa@gmail.com>
2021-04-16 17:33:35 -0300

    test: Should not return created user account sucess if alredy exists user

--- src/main/routes/express/index.test.ts
+++ src/main/routes/express/index.test.ts
@@ -4,6 +4,12 @@ import app from "../../app";
 import { cleanColumn } from "../../../infra/repositories/postgres/knex/helpers/knex-helpers";
 
 describe("User routes tests", () => {
+  const validData = {
+    name: "Edmilson",
+    email: "test@gmail.com",
+    password: "123456789",
+  };
+
   beforeEach(async () => {
     await cleanColumn("users");
   });
@@ -11,11 +17,17 @@ describe("User routes tests", () => {
   test("Should return created user account sucess", async () => {
     await request(app)
       .post("/api/register")
-      .send({
-        name: "Edmilson",
-        email: "test@gmail.com",
-        password: "123456789",
-      }).expect(201);
+      .send(validData).expect(201);
+  });
+
+  test("Should not return created user account sucess if alredy exists user", async () => {
+    await request(app)
+      .post("/api/register")
+      .send(validData);
+
+    await request(app)
+      .post("/api/register")
+      .send(validData).expect(401);
   });
 
   test("Should not return created user account sucess with invalid email", async () => {
