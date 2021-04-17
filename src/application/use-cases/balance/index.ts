2746e909bf6110adbb5ed50be244bbb31cc3889e
edmilson-dk <droidvidaboa@gmail.com>
2021-04-17 18:58:29 -0300

    feat: created balance use cases implementation

---
+++ src/application/use-cases/balance/index.ts
@@ -0,0 +1,39 @@
+import { IBalanceUseCases } from "../../../domain/use-cases/balance";
+import { IBalanceRepository } from "../../repositories/balance";
+import {  BalanceDataDTO } from "../../../domain/dtos/balance";
+import { Either, left, right } from "../../../shared/either";
+import { JoinedError } from "../../../domain/entities/Balance/errors/joined-error";
+import { LeftValueError } from "../../../domain/entities/Balance/errors/left-error";
+import Balance from "../../../domain/entities/Balance";
+import { AddBalanceResponse } from "./responses/add-balance";
+import BalanceMap from "../../../domain/dtos/balance/balance-map";
+
+class BalanceUseCases implements IBalanceUseCases {
+  private balanceRepository: IBalanceRepository;
+
+  constructor(balanceRepository: IBalanceRepository) {
+    this.balanceRepository = balanceRepository;
+  }
+
+  async add(data: BalanceDataDTO): Promise<AddBalanceResponse> {
+    const balanceOrError: Either<JoinedError | LeftValueError, Balance> = Balance.create(data);
+
+    if (balanceOrError.isLeft()) {
+      return left(balanceOrError.value);
+    }
+
+    const balance: Balance = balanceOrError.value;
+    const balanceData = BalanceMap.toPersist({ userId: data.userId, ...balance });
+
+    if (await this.balanceRepository.containBalance(data.userId)) {
+      await this.balanceRepository.updateBalance(balanceData);
+      return right(balanceData);
+    }
+
+    await this.balanceRepository.add(balanceData);
+
+    return right(balanceData);
+  }
+}
+
+export default BalanceUseCases;
