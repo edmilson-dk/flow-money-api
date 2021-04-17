03d5534258723375c538382500d210406fa0cda7
edmilson-dk <droidvidaboa@gmail.com>
2021-04-17 18:52:53 -0300

    feat: created transactions entity title value object

---
+++ src/domain/entities/Transactions/values-object/title.ts
@@ -0,0 +1,33 @@
+import { Either, left, right } from "../../../../shared/either";
+import { InvalidTitleError } from "../errors/title-error";
+
+class Title {
+  private readonly title: string;
+
+  constructor(title: string) {
+    this.title = title;
+    Object.freeze(this);
+  }
+
+  get value(): string {
+    return this.title;
+  }
+
+  static create(title: string): Either<InvalidTitleError, Title> {
+    if (!Title.validate(title)) {
+      return left(new InvalidTitleError(title));
+    }
+
+    return right(new Title(title));
+  }
+
+  static validate(title: string): boolean {
+    if (!title || title.trim().length < 5 || title.trim().length > 255) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+export default Title;
