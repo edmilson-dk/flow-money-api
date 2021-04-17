112f329a45691479e0a7095d2a4c0b7266a47bdd
edmilson-dk <droidvidaboa@gmail.com>
2021-04-17 18:52:26 -0300

    feat: created transactions entity category value object

---
+++ src/domain/entities/Transactions/values-object/category.ts
@@ -0,0 +1,33 @@
+import { Either, left, right } from "../../../../shared/either";
+import { InvalidCategoryError } from "../errors/category-error";
+
+class Category {
+  private readonly category: string;
+
+  constructor(category: string) {
+    this.category = category;
+    Object.freeze(this);
+  }
+
+  get value(): string {
+    return this.category;
+  }
+
+  static create(category: string): Either<InvalidCategoryError, Category> {
+    if (!Category.validate(category)) {
+      return left(new InvalidCategoryError(category));
+    }
+
+    return right(new Category(category));
+  }
+
+  static validate(category: string): boolean {
+    if (!category || category.trim().length < 5 || category.trim().length > 80) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+export default Category;
