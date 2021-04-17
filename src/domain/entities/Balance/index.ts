4a51166ede50538069c413efe37a9572d27e8f86
edmilson-dk <droidvidaboa@gmail.com>
2021-04-17 18:57:18 -0300

    feat: created balance entity

--- src/domain/entities/Balance/index.ts
+++ src/domain/entities/Balance/index.ts
@@ -1,22 +1,36 @@
+import { Either, left, right } from "../../../shared/either";
 import { BalanceDomainDTO } from "../../dtos/balance";
+import { JoinedError } from "./errors/joined-error";
+import { LeftValueError } from "./errors/left-error";
+import Joined from "./values-objects/joined";
+import LeftValue from "./values-objects/left";
 
 class Balance {
-  public joined: number;
-  public left: number;
+  public joined: Joined;
+  public left: LeftValue;
   public total: number;
 
-  constructor({ joined, left, total }: BalanceDomainDTO) {
+  constructor(joined: Joined, left: LeftValue ) {
     this.joined = joined;
     this.left = left;
-    this.total = total;
+    this.total = Number(joined)- Number(left);
   }
 
-  getValues() {
-    return {
-      joined: this.joined,
-      left: this.left,
-      total: this.total,
+  static create(data: BalanceDomainDTO): Either<JoinedError | LeftValueError, Balance> {
+    const joinedOrError: Either<JoinedError, Joined> = Joined.create(data.joined);
+    const leftOrError: Either<LeftValueError, LeftValue> = LeftValue.create(data.left);
+
+    if (joinedOrError.isLeft()) {
+      return left(new JoinedError(data.joined));
     }
+    if (leftOrError.isLeft()) {
+      return left(new LeftValueError(data.left));
+    }
+
+    const joined: Joined = joinedOrError.value;
+    const leftValue: LeftValue = leftOrError.value;
+
+    return right(new Balance(joined, leftValue));
   }
 }
 
