5c1fc5bbcd06d90a5d66c6a1d36eeaba013b81be
edmilson-dk <droidvidaboa@gmail.com>
2021-04-17 19:10:57 -0300

    refactor: added user id value object in create balance method

--- src/domain/entities/Balance/index.ts
+++ src/domain/entities/Balance/index.ts
@@ -2,23 +2,28 @@ import { Either, left, right } from "../../../shared/either";
 import { BalanceDomainDTO } from "../../dtos/balance";
 import { JoinedError } from "./errors/joined-error";
 import { LeftValueError } from "./errors/left-error";
+import { UserIdError } from "./errors/user-id-error";
 import Joined from "./values-objects/joined";
 import LeftValue from "./values-objects/left";
+import UserId from "./values-objects/user-id";
 
 class Balance {
   public joined: Joined;
   public left: LeftValue;
   public total: number;
+  public userId: UserId;
 
-  constructor(joined: Joined, left: LeftValue ) {
+  constructor(joined: Joined, left: LeftValue, userId: UserId) {
     this.joined = joined;
     this.left = left;
-    this.total = Number(joined)- Number(left);
+    this.userId = userId;
+    this.total = Number(joined) - Number(left);
   }
 
   static create(data: BalanceDomainDTO): Either<JoinedError | LeftValueError, Balance> {
     const joinedOrError: Either<JoinedError, Joined> = Joined.create(data.joined);
     const leftOrError: Either<LeftValueError, LeftValue> = LeftValue.create(data.left);
+    const userIdOrError: Either<UserIdError, UserId> = UserId.create(data.userId);
 
     if (joinedOrError.isLeft()) {
       return left(new JoinedError(data.joined));
@@ -26,11 +31,15 @@ class Balance {
     if (leftOrError.isLeft()) {
       return left(new LeftValueError(data.left));
     }
+    if (userIdOrError.isLeft()) {
+      return left(new UserIdError(data.userId));
+    }
 
     const joined: Joined = joinedOrError.value;
     const leftValue: LeftValue = leftOrError.value;
+    const userId: UserId = userIdOrError.value;
 
-    return right(new Balance(joined, leftValue));
+    return right(new Balance(joined, leftValue, userId));
   }
 }
 
