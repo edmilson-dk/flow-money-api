e512e685ecab4de96603a2ff721986bdb1d1489a
edmilson-dk <droidvidaboa@gmail.com>
2021-04-18 16:27:13 -0300

    feat: added new validation in joined balance value object

--- src/domain/entities/Balance/values-objects/joined.spec.ts
+++ src/domain/entities/Balance/values-objects/joined.spec.ts
@@ -2,6 +2,6 @@ import Joined from "./joined";
 
 describe("Joined value object validator", () => {
   test("Should not create a joined object with value negative", () => {
-    expect(Joined.validate(0.0333)).toBe(false);
+    expect(Joined.validate(-10)).toBe(false);
   });
 });
