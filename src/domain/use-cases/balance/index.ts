b0d5dcc5c0b7f5ba2258ae6e3ed9a3251ebedf46
edmilson-dk <droidvidaboa@gmail.com>
2021-04-21 21:52:27 -0300

    feat: added new type in add balance use case method type

--- src/domain/use-cases/balance/index.ts
+++ src/domain/use-cases/balance/index.ts
@@ -1,7 +1,9 @@
 import { AddBalanceResponse } from "../../../application/use-cases/balance/responses/add-balance";
+import { GetBalanceResponse } from "../../../application/use-cases/balance/responses/get-balance";
 import {  BalanceDataDTO } from "../../dtos/balance";
 
 export interface IBalanceUseCases {
-  add: ({ joined, left, userId }: BalanceDataDTO) => Promise<AddBalanceResponse>;
+  add: (data: BalanceDataDTO) => Promise<AddBalanceResponse>;
+  getBalance: (userId: string) => Promise<GetBalanceResponse>;
   //getTotal: (userId: string) => Promise<number>;
 }
