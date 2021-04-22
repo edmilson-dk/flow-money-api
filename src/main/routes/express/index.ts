bb42e0085ca14e3305379ca04b21182dff750255
edmilson-dk <droidvidaboa@gmail.com>
2021-04-21 21:53:52 -0300

    feat: added auth user middleware

--- src/main/routes/express/index.ts
+++ src/main/routes/express/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import authMiddleware from "./middlewares/auth-middleware";
+import transactionRoutes from "./transaction";
 import userRoutes from "./user";
 
 const routes = Router();
 
+routes.use("/session", authMiddleware);
 routes.use(userRoutes);
+routes.use("/session", transactionRoutes);
 
 export default routes;
