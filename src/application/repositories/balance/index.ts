da27e0a7c9c94dedb4c2684eddc9c7f7a3ca3854
edmilson-dk <droidvidaboa@gmail.com>
2021-04-21 21:45:58 -0300

    refactor: removed unused methods

--- src/application/repositories/balance/index.ts
+++ src/application/repositories/balance/index.ts
@@ -1,8 +1,8 @@
 import { BalancePersistDTO } from "../../../domain/dtos/balance";
 
 export interface IBalanceRepository {
-  add: ({ total, joined, left, userId }: BalancePersistDTO) => Promise<void>;
+  add: (data: BalancePersistDTO) => Promise<void>;
+  getBalance: (userId: string) => Promise<BalancePersistDTO>;
   containBalance: (userId: string) => Promise<boolean>;
   updateBalance: (data: BalancePersistDTO) => Promise<void>;
-  deleteBalance: (userId: string) => Promise<void>;
 }
