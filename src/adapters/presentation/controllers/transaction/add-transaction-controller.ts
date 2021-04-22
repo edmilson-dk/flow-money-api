b7ff6c2447086c427e31662235997c9f019c06ad
edmilson-dk <droidvidaboa@gmail.com>
2021-04-21 21:55:37 -0300

    feat: created add transaction controller

---
+++ src/adapters/presentation/controllers/transaction/add-transaction-controller.ts
@@ -0,0 +1,39 @@
+import { AddTransactionResponse } from "../../../../application/use-cases/transaction/responses/add-transaction";
+import TransactionMap from "../../../../domain/dtos/transaction/transaction-map";
+import { ITransactionUseCases } from "../../../../domain/use-cases/transaction";
+import { badRequest, ok, serverError } from "../../http/http-response-type";
+import { HttpRequest, HttpResponse } from "../../http/ports/http";
+import { BaseController } from "../baseControler";
+import { MissingParamError } from "../errors/missing-params-error";
+
+export class AddTransactionController implements BaseController {
+  private readonly transactionUseCases: ITransactionUseCases;
+
+  constructor(transactionUseCases: ITransactionUseCases) {
+    this.transactionUseCases = transactionUseCases;
+  }
+
+  async execute(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { isDecrement, value, category, title } = httpRequest.body;
+      const userId = httpRequest.rest.userId;
+
+      if (!isDecrement || !value || !category || !title) {
+        return badRequest(new MissingParamError(), 401);
+      }
+
+      const addTransactionResponse: AddTransactionResponse = await this.transactionUseCases.add({ isDecrement, value, category, title, userId });
+
+      if (addTransactionResponse.isLeft()) {
+        return badRequest(addTransactionResponse.value, 401);
+      }
+
+      const transactionDto = TransactionMap.toDTO(addTransactionResponse.value);
+      
+      return ok(transactionDto, 201);
+    } catch (err) {
+      console.log(err)
+      return serverError(err.message);
+    }
+  }
+}
