cf4d01315ced1dd7f8ba6c73d482ce680f976db2
edmilson-dk <droidvidaboa@gmail.com>
2021-04-21 21:46:35 -0300

    refactor: removed unused methods

--- src/application/use-cases/balance/index.ts
+++ src/application/use-cases/balance/index.ts
@@ -7,6 +7,8 @@ import { LeftValueError } from "../../../domain/entities/Balance/errors/left-err
 import Balance from "../../../domain/entities/Balance";
 import { AddBalanceResponse } from "./responses/add-balance";
 import BalanceMap from "../../../domain/dtos/balance/balance-map";
+import { GetBalanceResponse } from "./responses/get-balance";
+import { NotExistsBalanceError } from "./errors/not-exists-balance-error";
 
 class BalanceUseCases implements IBalanceUseCases {
   private balanceRepository: IBalanceRepository;
@@ -17,22 +19,31 @@ class BalanceUseCases implements IBalanceUseCases {
 
   async add(data: BalanceDataDTO): Promise<AddBalanceResponse> {
     const balanceOrError: Either<JoinedError | LeftValueError, Balance> = Balance.create(data);
-
+   
     if (balanceOrError.isLeft()) {
       return left(balanceOrError.value);
     }
 
     const balance: Balance = balanceOrError.value;
-    const balanceData = BalanceMap.toPersist({ ...balance });
+    const balanceData = BalanceMap.toPersist(balance.getValues());
 
     if (await this.balanceRepository.containBalance(data.userId)) {
       await this.balanceRepository.updateBalance(balanceData);
       return right(balanceData);
     }
-
+    
     await this.balanceRepository.add(balanceData);
-
     return right(balanceData);
+  }
+
+  async getBalance(userId: string): Promise<GetBalanceResponse> {
+    if (!(await this.balanceRepository.containBalance(userId))) {
+      return left(new NotExistsBalanceError(userId));
+    }
+
+    const balance = await this.balanceRepository.getBalance(userId);
+
+    return right(BalanceMap.toPersist(balance));
   }
 }
 
