9f7e3b08332841d72edea1441e39cde242bbd74e
edmilson-dk <droidvidaboa@gmail.com>
2021-04-21 22:28:37 -0300

    feat: created exists transaction by title method type repository

--- src/application/repositories/transaction/index.ts
+++ src/application/repositories/transaction/index.ts
@@ -2,4 +2,5 @@ import { TransactionPersistDTO } from "../../../domain/dtos/transaction";
 
 export interface ITransactionRepository {
   add: (data: TransactionPersistDTO) => Promise<void>;
+  existsTransactionByTitle: (title: string) => Promise<boolean>;
 }
