3d6d393662a7bcda34dafa488755d536ee643082
edmilson-dk <droidvidaboa@gmail.com>
2021-04-21 22:30:26 -0300

    feat: added balances routes

--- src/main/routes/express/index.ts
+++ src/main/routes/express/index.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import balanceRoutes from "./balance";
 import authMiddleware from "./middlewares/auth-middleware";
 import transactionRoutes from "./transaction";
 import userRoutes from "./user";
 
 const routes = Router();
 
-routes.use("/session", authMiddleware);
 routes.use(userRoutes);
+
+// authenticate user middleware
+routes.use("/session", authMiddleware);
+
 routes.use("/session", transactionRoutes);
+routes.use("/session", balanceRoutes);
 
 export default routes;
