988b2b50e7ab44e629ef90222daf9705ee8466cf
edmilson-dk <droidvidaboa@gmail.com>
2021-04-21 21:47:56 -0300

    feat: added to domain method

--- src/domain/dtos/balance/balance-map.ts
+++ src/domain/dtos/balance/balance-map.ts
@@ -1,11 +1,27 @@
-import { BalancePersistDTO } from ".";
+import { BalanceDomainDTO, BalanceDTO, BalancePersistDTO } from ".";
 
 class BalanceMap {
   static toPersist(data: any): BalancePersistDTO {
     return {
       joined: Number(data.joined),
       left: Number(data.left),
-      total: data.total,
+      total: Number(data.total),
+      userId: data.userId,
+    };
+  }
+
+  static toDTO(data: any): BalanceDTO {
+    return {
+      joined: Number(data.joined),
+      left: Number(data.left),
+      total: Number(data.total),
+    }
+  }
+
+  static toDomain(data: any): BalanceDomainDTO {
+    return {
+      joined: Number(data.joined),
+      left: Number(data.left),
       userId: data.userId,
     };
   }
