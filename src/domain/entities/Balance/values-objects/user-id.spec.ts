b922042f209ddb7550a0d1951e11334ca3b3a7f0
edmilson-dk <droidvidaboa@gmail.com>
2021-04-24 20:56:59 -0300

    test: Should not create user id with empty value

---
+++ src/domain/entities/Balance/values-objects/user-id.spec.ts
@@ -0,0 +1,11 @@
+import UserId from "./user-id";
+
+describe("User Id value object validate", () => {
+  test("Should not create user id with empty value", () => {
+    expect(UserId.validate("")).toBe(false);
+  });
+
+  test("Should not create user id object with min 1 char", () => {
+    expect(UserId.validate("1")).toBe(false);
+  });
+});
