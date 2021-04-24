9eb0833ae046bb7c60452a866d052f71e96bbddb
edmilson-dk <droidvidaboa@gmail.com>
2021-04-24 20:54:25 -0300

    test: Should not create a joined object with empty value

--- src/domain/entities/Balance/values-objects/joined.spec.ts
+++ src/domain/entities/Balance/values-objects/joined.spec.ts
@@ -4,4 +4,8 @@ describe("Joined value object validator", () => {
   test("Should not create a joined object with value negative", () => {
     expect(Joined.validate(-10)).toBe(false);
   });
+
+  test("Should not create a joined object with empty value", () => {
+    expect(Joined.validate(null)).toBe(false);
+  });
 });
