5a55dada5c879b7e37e4d1122d933efb7ab3dcda
edmilson-dk <droidvidaboa@gmail.com>
2021-04-24 20:56:33 -0300

    test: Should not create a left object with value negative

---
+++ src/domain/entities/Balance/values-objects/left.spec.ts
@@ -0,0 +1,11 @@
+import Left from "./left";
+
+describe("Left value object validator", () => {
+  test("Should not create a left object with value negative", () => {
+    expect(Left.validate(-10)).toBe(false);
+  });
+
+  test("Should not create a left object with empty value", () => {
+    expect(Left.validate(null)).toBe(false);
+  });
+});
