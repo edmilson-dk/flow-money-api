32fb535f79fdcca02951c5678a644a780e7969d3
edmilson-dk <droidvidaboa@gmail.com>
2021-04-27 10:31:52 -0300

    test: Should not create a category longer than 80 characters

--- src/domain/entities/Transaction/values-object/category.spec.ts
+++ src/domain/entities/Transaction/values-object/category.spec.ts
@@ -4,4 +4,7 @@ describe("Category value object validator", () => {
   test("Should not create a category of less than 3 characters", () => {
     expect(Category.validate("aa")).toBeFalsy();
   }); 
+  test("Should not create a category longer than 80 characters", () => {
+    expect(Category.validate("aa".repeat(81))).toBeFalsy();
+  });
 });
