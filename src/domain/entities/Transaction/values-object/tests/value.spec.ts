efa03804e12086dec570333043333627da1dc3c8
edmilson-dk <droidvidaboa@gmail.com>
2021-04-27 10:59:10 -0300

    test: Should not create a value object with a null value

--- src/domain/entities/Transaction/values-object/tests/value.spec.ts
+++ src/domain/entities/Transaction/values-object/tests/value.spec.ts
@@ -4,4 +4,7 @@ describe("Value value object validator", () => {
   test("Should not create a value object with a negative number", () => {
     expect(Value.validate(-10)).toBeFalsy();
   });
+  test("Should not create a value object with a null value", () => {
+    expect(Value.validate(null)).toBeFalsy();
+  });
 });
