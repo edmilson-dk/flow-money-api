bf7f1a87cff16ece8cc09116c78dc95c9e4166fc
edmilson-dk <droidvidaboa@gmail.com>
2021-04-27 10:36:52 -0300

    test: Should not create a title longer than 80 characters

--- src/domain/entities/Transaction/values-object/title.spec.ts
+++ src/domain/entities/Transaction/values-object/title.spec.ts
@@ -4,4 +4,7 @@ describe("Title value object validator", () => {
   test("Should not create a title of less than 3 characters", () => {
     expect(Title.validate("aa")).toBeFalsy();
   });
+  test("Should not create a title longer than 80 characters", () => {
+    expect(Title.validate("aa".repeat(81))).toBeFalsy();
+  });
 });
