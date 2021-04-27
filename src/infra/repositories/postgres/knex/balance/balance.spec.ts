d808a21be5e5cb60d4793d2a3a41029e38638573
edmilson-dk <droidvidaboa@gmail.com>
2021-04-27 11:40:24 -0300

    test: Should add an balance data in database

---
+++ src/infra/repositories/postgres/knex/balance/balance.spec.ts
@@ -0,0 +1,31 @@
+import { generateId } from "../../../../../utils/generateId";
+import BalanceRepository from "./index";
+import { cleanColumn } from "../helpers/knex-helpers";
+import BalanceMap from "../../../../../domain/dtos/balance/balance-map";
+
+function generateData(left = 0, joined = 0) {
+  return {
+    userId: generateId(),
+    left,
+    joined,
+    total: joined - left, 
+  };
+}
+
+describe("Balance repository tests", () => {
+  const balanceRepository = new BalanceRepository();
+
+  beforeEach(async () => {
+    await cleanColumn("balances");
+  });
+
+  test("Should add an balance data in database", async () => {
+    const data = generateData();
+    const userId = data.userId;
+    
+    await balanceRepository.add(data);
+    const stored = await balanceRepository.getBalance(userId);
+
+    expect(BalanceMap.toPersist(stored)).toEqual(data);
+  });
+});
