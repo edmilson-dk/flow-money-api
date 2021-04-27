e3adee2ceaa181b04a66e91b0db9e9fbdce85274
edmilson-dk <droidvidaboa@gmail.com>
2021-04-27 11:15:08 -0300

    refactor: removed test

--- src/domain/entities/Balance/values-objects/tests/user-id.spec.ts
+++ src/domain/entities/Balance/values-objects/tests/user-id.spec.ts
@@ -4,8 +4,4 @@ describe("User Id value object validate", () => {
   test("Should not create user id with empty value", () => {
     expect(UserId.validate("")).toBe(false);
   });
-
-  test("Should not create user id object with min 1 char", () => {
-    expect(UserId.validate("1")).toBe(false);
-  });
 });
