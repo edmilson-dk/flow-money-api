25254d1df0903da36ee80acb050e356ffc1d4977
edmilson-dk <droidvidaboa@gmail.com>
2021-04-27 10:52:48 -0300

    test: Should not create a user ID object with a null value

--- src/domain/entities/Transaction/values-object/tests/user-id.spec.ts
+++ src/domain/entities/Transaction/values-object/tests/user-id.spec.ts
@@ -4,4 +4,7 @@ describe("UserId value object validator", () => {
   test("Should not create a user-id object with an empty string", () => {
     expect(UserId.validate("")).toBeFalsy();
   });
+  test("Should not create a user ID object with a null value", () => {
+    expect(UserId.validate(null)).toBeFalsy();
+  });
 });
