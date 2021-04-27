701455357ad11e1e9a9fecb105022db97422ee07
edmilson-dk <droidvidaboa@gmail.com>
2021-04-27 11:05:48 -0300

    test: Should not create an transaction entity with invalid is-decrement value

--- src/domain/entities/Transaction/index.spec.ts
+++ src/domain/entities/Transaction/index.spec.ts
@@ -1,5 +1,6 @@
 import { left } from "../../../shared/either";
 import { InvalidCategoryError } from "./errors/category-error";
+import { InvalidIsDecrementError } from "./errors/is-decrement-error";
 import Transaction from "./index";
 
 describe("Create a transaction entity tests", () => {
@@ -11,8 +12,12 @@ describe("Create a transaction entity tests", () => {
     value: 1200
   };
 
-  test("Shoult not create an transaction entity with invalid category value", () => {
+  test("Should not create an transaction entity with invalid category value", () => {
     const data = Transaction.create({ ...transactonData, category: "" });
     expect(data).toEqual(left(new InvalidCategoryError("")));
+  }); 
+  test("Should not create an transaction entity with invalid is-decrement value", () => {
+    const data = Transaction.create({ ...transactonData, isDecrement: null });
+    expect(data).toEqual(left(new InvalidIsDecrementError(null)));
   }); 
 });
