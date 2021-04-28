dfc1eabc31e64ceae60b82994307ad645847ea70
edmilson-dk <droidvidaboa@gmail.com>
2021-04-28 14:58:56 -0300

    test: Should add balance in database

---
+++ src/application/use-cases/balance/index.spec.ts
@@ -0,0 +1,26 @@
+import BalanceUseCases from "./index";
+import BalanceRepository from "../../../infra/repositories/postgres/knex/balance";
+import { cleanColumn } from "../../../infra/repositories/postgres/knex/helpers/knex-helpers";
+import { generateId } from "../../../utils/generateId";
+
+function generateData(id: string, left = 0, joined = 0) {
+  return {
+    userId: id,
+    left,
+    joined,
+  }
+}
+
+describe("Balance use cases tests", () => {
+  const balanceUseCases = new BalanceUseCases(new BalanceRepository());
+
+  beforeEach(async () => {
+    await cleanColumn("balances");
+  });
+
+  test("Should add balance in database", async () => {
+    const data = generateData(generateId());
+    const balance = await balanceUseCases.add(data);
+    expect(balance.isRight()).toBeTruthy();
+  });
+});
