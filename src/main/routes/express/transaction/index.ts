ee267e3382b11c3d8d9bd47e15aacd5d69b001a1
edmilson-dk <droidvidaboa@gmail.com>
2021-04-29 17:28:22 -0300

    feat: created drop transaction controller router

--- src/main/routes/express/transaction/index.ts
+++ src/main/routes/express/transaction/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { adaptRoute } from "../../../adapters/express-adapter";
-import { makeAddTransactionController } from "../../../factories/transaction/add-transaction";
+import { makeAddTransactionController, makeDropTransactionController } from "../../../factories/transaction";
 
 const transactionRoutes = Router();
 
 transactionRoutes.post("/create/transaction", adaptRoute(makeAddTransactionController()));
+transactionRoutes.delete("/drop/transaction/:id", adaptRoute(makeDropTransactionController()));
 
 export default transactionRoutes;
