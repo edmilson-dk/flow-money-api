29deaeddd5ec98c78f4f374834cecfa73447865a
edmilson-dk <droidvidaboa@gmail.com>
2021-04-29 17:27:58 -0300

    feat: created drop transaction controller

---
+++ src/adapters/presentation/controllers/transaction/drop-transaction-controller.ts
@@ -0,0 +1,29 @@
+import { DropTransactionResponse } from "../../../../application/use-cases/transaction/responses/drop-transaction";
+import { ITransactionUseCases } from "../../../../domain/use-cases/transaction";
+import { badRequest, ok, serverError } from "../../http/http-response-type";
+import { HttpRequest, HttpResponse } from "../../http/ports/http";
+import { BaseController } from "../baseControler";
+
+export class DropTransactionController implements BaseController {
+  private readonly transactionUseCases: ITransactionUseCases;
+
+  constructor(transactionUseCases: ITransactionUseCases) {
+    this.transactionUseCases = transactionUseCases;
+  }
+
+  async execute(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try { 
+      const { id } = httpRequest.params;
+
+      const deletedTransaction: DropTransactionResponse = await this.transactionUseCases.dropTransaction(id);
+
+      if (deletedTransaction.isLeft()) {
+        return badRequest(deletedTransaction.value, 404);
+      }
+
+      return ok(deletedTransaction.value, 200);
+    } catch (err) {
+      return serverError(err.message);
+    }
+  }
+}
