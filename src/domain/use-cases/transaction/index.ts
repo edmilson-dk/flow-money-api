334e5facd79b3044ecf059acca2837cb45abfe83
edmilson-dk <droidvidaboa@gmail.com>
2021-04-29 17:31:10 -0300

    empty

--- src/domain/use-cases/transaction/index.ts
+++ src/domain/use-cases/transaction/index.ts
@@ -1,6 +1,8 @@
 import { AddTransactionResponse } from "../../../application/use-cases/transaction/responses/add-transaction";
+import { DropTransactionResponse } from "../../../application/use-cases/transaction/responses/drop-transaction";
 import { TransactionDataDTO } from "../../dtos/transaction";
 
 export interface ITransactionUseCases {
   add: (data: TransactionDataDTO) => Promise<AddTransactionResponse>;
+  dropTransaction: (id: string) => Promise<DropTransactionResponse>;
 }
