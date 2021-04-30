97974643573492f221e66ca2b50e61a0a293bb87
edmilson-dk <droidvidaboa@gmail.com>
2021-04-29 22:21:07 -0300

    feat: created get transactions controller

---
+++ src/adapters/presentation/controllers/transaction/get-transactions-controller.ts
@@ -0,0 +1,26 @@
+import { GetTransactionsResponse } from "../../../../application/use-cases/transaction/responses/get-transactions";
+import { ITransactionUseCases } from "../../../../domain/use-cases/transaction";
+import { ok, serverError } from "../../http/http-response-type";
+import { HttpRequest, HttpResponse } from "../../http/ports/http";
+import { BaseController } from "../baseControler";
+
+export class GetTransactionsController implements BaseController {
+  private readonly transactionUseCases: ITransactionUseCases;
+
+  constructor(transactionUseCases: ITransactionUseCases) {
+    this.transactionUseCases = transactionUseCases;
+  }
+
+  async execute(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try { 
+      const { page } = httpRequest.query;
+      const userId = httpRequest.rest.userId;
+
+      const transactions: GetTransactionsResponse = await this.transactionUseCases.getTransactions(userId, page);
+
+      return ok(transactions, 200);
+    } catch (err) {
+      return serverError(err.message);
+    }
+  }
+}
