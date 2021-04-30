f05bc46e86e9a5848659d0935d1215363bb8a966
edmilson-dk <droidvidaboa@gmail.com>
2021-04-29 22:21:35 -0300

    feat: added get transactions router

--- src/main/routes/express/transaction/index.ts
+++ src/main/routes/express/transaction/index.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { adaptRoute } from "../../../adapters/express-adapter";
-import { makeAddTransactionController, makeDropTransactionController } from "../../../factories/transaction";
+import { 
+  makeAddTransactionController, 
+  makeDropTransactionController, 
+  makeGetTransactionsController
+ } from "../../../factories/transaction";
 
 const transactionRoutes = Router();
 
 transactionRoutes.post("/create/transaction", adaptRoute(makeAddTransactionController()));
 transactionRoutes.delete("/drop/transaction/:id", adaptRoute(makeDropTransactionController()));
+transactionRoutes.get("/transactions", adaptRoute(makeGetTransactionsController()));
 
 export default transactionRoutes;
