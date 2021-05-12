058dfaa1002aff62d44f8cf2d215dcc9835d7ebf
edmilson-dk <droidvidaboa@gmail.com>
2021-05-11 22:00:15 -0300

    refactor: added new response type

--- src/application/use-cases/transaction/responses/drop-transaction.ts
+++ src/application/use-cases/transaction/responses/drop-transaction.ts
@@ -1,5 +1,5 @@
-import { TransactionDTO } from "../../../../domain/dtos/transaction";
+import { TransactionPersistDTO } from "../../../../domain/dtos/transaction";
 import { Either } from "../../../../shared/either";
 import { NotExistsTransactionError } from "../errors/not-exists-transaction";
 
-export type DropTransactionResponse = Either<NotExistsTransactionError, TransactionDTO>;
+export type DropTransactionResponse = Either<NotExistsTransactionError, TransactionPersistDTO>;
